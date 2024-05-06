51da75c8a70f757ae5f5e07f0e35ec0a81d381ab
inderpreet <inderpreet>
2024-05-07 02:33:59 +0530

    Next js boilerplate

---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+; "use client"
+import React from 'react';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+
+
+
+const Header: React.FC = () => {
+    const { setTheme } = useTheme();
+    return (
+        <header className="text-gray-600 body-font">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a href="#" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    <span className="ml-3 text-xl dark:text-white">Next js</span>
+                </a>
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
+                    <a href="#" className="mr-5 hover:text-gray-900">Home</a>
+                    <a href="#" className="mr-5 hover:text-gray-900">About</a>
+                    <a href="#" className="mr-5 hover:text-gray-900">Contact us</a>
+                </nav>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                            <span className="sr-only">Toggle theme</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => setTheme("light")}>
+                            Light
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("dark")}>
+                            Dark
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("system")}>
+                            System
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+
+        </header>
+    );
+}
+
+export default Header;
